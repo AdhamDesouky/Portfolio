58e960aa6fde65feceb6fcf0de1c38e5d11c24e3
Adham Desouky <126396682+AdhamDesouky@users.noreply.github.com>
2025-04-17 16:27:53 +0200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Adham Desouky",
-  profile: '/profile.Webp',
+  profile: '/profile.png',
   designation: "Biomedical Informatics Student | AI & Full-Stack Developer",
   description: "Biomedical Informatics undergraduate at Nile University, currently working on a thesis titled 'AI-Powered Diagnostics for Breast Implant Health.' Experienced in building AI healthcare tools, full-stack web systems, and data-driven solutions with a real-world clinical focus. Passionate about bridging the gap between clinical needs and software solutions.",
   email: 'adhamdesoukyy@gmail.com',
